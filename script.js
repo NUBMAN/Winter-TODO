c5271583af5aa3a9416a81bde9dcd7523cb26b63
NubMan <pechkursasha@gmail.com>
2024-01-26 18:20:08 +0200

    feat: add button that add list elements

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     //get elements that we need
-    delete_btn = document.getElementsByClassName('delete-btn'); 
+    const delete_btns = document.getElementsByClassName('delete-btn'); 
+    add_btn = document.getElementById('add-btn');
+    todo_list = document.getElementById('list');
     
 
     //function that deletes btn on click
-    Array.from(delete_btn).forEach((delete_btn) => {
+    Array.from(delete_btns).forEach((delete_btn) => {
         delete_btn.addEventListener('click', (e) =>{
             const todo_item = e.currentTarget.closest('.list-item')
             if(todo_item){
@@ -14,6 +16,45 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     //function that adds new list items
+    add_btn.addEventListener('click', () => {
+        //create and add empty list item
+        const list_todo_elem = document.createElement('li');
+        list_todo_elem.className = 'list-item'
 
+        const todo_checkbox = document.createElement('input');
+        todo_checkbox.type = 'checkbox';
+        todo_checkbox.className = 'custom-checkbox';
+
+        const span_item = document.createElement('span');
+        span_item.className = 'list-item'
+        span_item.style.marginLeft = "10px";
+        span_item.style.marginRight = "10px";
+        span_item.textContent = document.querySelector('#user_input').value;
+
+        const delete_btn = document.createElement('button');
+        const img_icon = document.createElement('img')
+        img_icon.src = '/assets/clear-svgrepo-com.svg'
+        img_icon.style.width = '15px';
+
+        delete_btn.className = 'delete-btn'
+        delete_btn.addEventListener('click', e=> {
+            const todo_item = delete_btn.closest('.list-item');
+            if(todo_item){
+                todo_item.remove()
+            }
+        })
+
+        delete_btn.appendChild(img_icon);
+
+
+
+
+        list_todo_elem.appendChild(todo_checkbox);
+        list_todo_elem.appendChild(span_item);
+        list_todo_elem.appendChild(delete_btn);
+        todo_list.appendChild(list_todo_elem);
+
+
+    });
 
 });
